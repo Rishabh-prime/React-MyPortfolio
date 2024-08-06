5b619015da3a36359b8dddb59cec6eebcac14848
Clashprime <rishabh1616thakurQgmail.com>
2024-08-06 22:41:34 +0530

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/React-MyPortfolio/', // Set this to your repository name
+  base: '/React-RishabhPotfolio/', // Set this to your repository name
   plugins: [react()],
 });
